125e3e0262dd81719d7d05a20dfd09fc9deb650a
codyduong <duongcody507@gmail.com>
2023-04-25 15:25:46 -0500

    Add newline

--- pi/prog.py
+++ pi/prog.py
@@ -9,7 +9,7 @@ with open("commands.csv", newline="") as csvfile:
     for row in reader:
         print(row)
         # angle, speed, duration
-        command = f"a:{row[0]};s:{row[1]};d:{row[2]};\n"
+        command = f"\na:{row[0]};s:{row[1]};d:{row[2]};\n"
         ser1.write(command.encode())
         # time.sleep(float(row[2]))
 
