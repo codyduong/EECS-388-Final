60625da7b689ae0983623f2c4d1aa048ed608a8a
codyduong <duongcody507@gmail.com>
2023-04-25 14:01:08 -0500

    Init prog.py

---
+++ pi/prog.py
@@ -0,0 +1,16 @@
+import serial
+import time
+import csv
+
+ser1 = serial.Serial("/dev/ttyAMA1", 115200)
+
+with open("commands.csv", newline="") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
+        # angle, speed, duration
+        command = f"a:{row[0]};s:{row[1]};d{row[2]}"
+        ser1.write(command.encode())
+        time.sleep(float(row[3]))
+
+ser1.close()
