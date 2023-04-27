fc42db5e4f46a9fd8a088b1c77171840b77466a0
codyduong <duongcody507@gmail.com>
2023-04-27 14:14:23 -0500

    Close serial even if exiting early

--- pi/progBonus.py
+++ pi/progBonus.py
@@ -12,13 +12,17 @@ def send_command(name: str, row: List[str], serial: serial.Serial) -> None:
     serial.write(command.encode())  # type: ignore
 
 
-def schedule_commands(filename: str, scheduler: sched.scheduler, serial: serial.Serial) -> None:
+def schedule_commands(
+    filename: str, scheduler: sched.scheduler, serial: serial.Serial
+) -> None:
     with open(filename, newline="") as csvfile:
         delay: float = 0
         # remove first row
         rows: List[List[str]] = list(csv.reader(csvfile))[1:]
         for index, row in enumerate(rows):
-            scheduler.enter(delay, index, send_command, argument=(filename, row, serial))
+            scheduler.enter(
+                delay, index, send_command, argument=(filename, row, serial)
+            )
             # add new delay to scheduler
             delay += float(row[3])
 
@@ -34,6 +38,8 @@ def main(s1: serial.Serial, s2: serial.Serial) -> None:
 if __name__ == "__main__":
     s1: serial.Serial = serial.Serial("/dev/ttyAMA1", 115200)
     s2: serial.Serial = serial.Serial("/dev/ttyAMA2", 115200)
-    main(s1, s2)
-    s1.close()
-    s2.close()
+    try:
+        main(s1, s2)
+    finally:
+        s1.close()
+        s2.close()
